fa7ea03a134efe31225adee8f7184d3a9ded77b1
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-27 14:30:42 +0200

    first snack completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+/*
+Snack - Il software deve chiedere per 10 volte all’utente di inserire un numero
+Il programma stampa la somma di tutti i numeri inseriti.
+ */
+
+//dichiarazione array
+int numbers = 0;
+
+//richiesta all'user
+for(int i = 0; i<10;i++)
+{
+    Console.Write("Insert a number: ");
+    numbers += Int32.Parse(Console.ReadLine());
+
+}
+
+Console.WriteLine("The sum of your numbers is: {0}",numbers);
