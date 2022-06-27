a16cd1ebd43bf0aade11aa66cb4ee5aa0fd92098
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-27 15:38:05 +0200

    Snack 10 completed

--- Program.cs
+++ Program.cs
@@ -2,13 +2,51 @@
 //Console.WriteLine("Hello, World!");
 
 /*
+Snack 10
+Fai inserire un numero, che chiameremo N all'utente.
+Genera N, array ognuno formato da 10 num casuali tra 1 e 100.
+Ogni volta che ne crei uno stampalo a schermo.
+*/
+
+Console.Write("Insert a number: ");
+
+int N = Int32.Parse(Console.ReadLine());
+
+int[] arrayNumbers;
+
+Random rng = new Random();
+
+for (int nArrays = 0; nArrays < N; nArrays++)
+{
+    arrayNumbers = new int[10];
+
+    for(int i = 0; i < arrayNumbers.Length; i++)
+    {
+        arrayNumbers[i] = rng.Next(1, 100);
+    }
+    
+    for(int i = 0; i < arrayNumbers.Length; i++)
+    {
+        Console.Write(arrayNumbers[i] + ",");
+    }
+    Console.WriteLine();
+}
+
+
+
+return;
+
+/*
 Snack 5
 Il software chiede di inserire un numero all'utente
 Se il numero è pari, stampa il numero, se è dispari, stampa il successivo
  */
+
+//user input
 Console.WriteLine("Insert a number");
 int userNum = Int32.Parse(Console.ReadLine());
 
+//logica controllo input
 if (userNum % 2 == 0) {
     Console.Write($"Your number: {userNum}, is EVEN! ");
 }else
@@ -17,8 +55,6 @@ if (userNum % 2 == 0) {
 }
 
 
-
-return;
 
 
 /*
